fe2cc14756b29f0f90e93dc7b1c5094ce4e56abf
Saba Sakandelidze <saba.saka01@gmail.com>
2024-02-18 00:42:09 +0400

    for deploying

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import { PacmanLoader } from "react-spinners";
 
 export default function loading() {
   return (
-    <div className="absolute translate-x-1/2 translate-y-1/2 inset-1/2">
+    <div className="absolute translate-x-1/2  translate-y-1/2 inset-1/2">
       <PacmanLoader color="#36d7b7" />
     </div>
   );
